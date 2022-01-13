b09e571f5896ec173f9b774924025155712c724d
sim89 <simrenkaursoor@gmail.com>
2022-01-13 16:16:13 +0000

    added paths for about, home and aceventura pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,18 +7,21 @@ import AboutPage from "../BeetRoutePage";
 import AceVenturaPage from "../AceVenturaPage";
 
 function App() {
-  return (
-    <div className="App">
-      <nav className="nav-bar">
-        <h1>www.veg.com</h1>
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="aceVentura" element={<AceVenturaPage />} />
-      </Routes>
-    </div>
-  );
+   return (
+      <div className="App">
+         <nav className="nav-bar">
+            <h1>www.veg.com</h1>
+            <Link to="/about">About</Link>
+            <Link to="/">Home</Link>
+            <Link to="/aceVentura">AceVenturaPage</Link>
+         </nav>
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="aceVentura" element={<AceVenturaPage />} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
