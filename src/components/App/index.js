fabc8b906d8ad5eaa79db26853f028207428167f
Nadja <nadjagrothe@gmail.com>
2022-01-13 15:48:03 +0000

    installed & imported react routers

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 import HomePage from "../HomePage";
 import AboutPage from "../BeetRoutePage";
 import AceVenturaPage from "../AceVenturaPage";
 
 function App() {
-  return (
-    <div className="App">
-      <nav className="nav-bar">
-        <h1>www.veg.com</h1>
-      </nav>
-      <HomePage />
-      <AboutPage />
-      <AceVenturaPage />
-    </div>
-  );
+   return (
+      <div className="App">
+         <nav className="nav-bar">
+            <h1>www.veg.com</h1>
+         </nav>
+         <HomePage />
+         <AboutPage />
+         <AceVenturaPage />
+      </div>
+   );
 }
 
 export default App;
